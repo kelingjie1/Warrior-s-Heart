da5aa2915b6f000d5a4d012491a00f25b2821190
kelingjie1 <kelingjie1@126.com>
2015-01-18 14:14:13 +0800

    修改文件结构，增加假数据管理

---
+++ src/Assets/Scripts/Core/Network/FakeDataManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using game_proto;
+using ProtoBuf;
+
+public class FakeDataManager 
+{
+    static FakeDataManager m_instance;
+    public static FakeDataManager Instance
+    {
+        get
+        {
+            if (m_instance==null)
+            {
+                m_instance = new FakeDataManager();
+            }
+            return m_instance;
+        }
+    }
+
+    public void GetFakeDataAsync(IExtensible packet)
+    {
+        Debug.Log(packet.GetType());
+    }
+}
