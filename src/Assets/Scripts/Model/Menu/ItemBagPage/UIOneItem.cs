f6807aa94d08d7e3a1da9d0a6c91cadaa4b12139
sunstarzixin <sunstarzixin@163.com>
2015-02-01 18:03:48 +0800

    背包动态加载

---
+++ src/Assets/Scripts/Model/Menu/ItemBagPage/UIOneItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using game_proto;
+
+//ui 数据结构 用户数据拉取回来以后直接放到这
+public class UIOneItem : MonoBehaviour
+{
+    public UIOneItem()
+    {
+        m_BagItem = new BagItem();
+    }
+    //协议数据结构
+    public BagItem m_BagItem;
+    //maybe other
+
+    public static UIOneItem Instance
+    {
+        get
+        {
+            return ResourceManager.LoadGameObject("Prefab/Menu/ItemBagPage/OneItemBag").GetComponent<UIOneItem>();
+        }
+    }
+
+    void Awake()
+    {
+        int i = 0;
+        i = i + 1;
+    }
+
+}
